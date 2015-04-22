1e63d1fcb7b12f3961b6524d8ec84a4ef3731736
Pikadude No. 1 <PikadudeNo1@users.noreply.github.com>
2015-04-22 05:08:58 -0700

    resultServlet: Changed POST to GET and wrote a plan for inplementing HTML output

--- src/resultServlet.java
+++ src/resultServlet.java
@@ -3,9 +3,12 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
 //incomplete
+/* This assumes that it will run as the top-level file, and includes results.jsp. But our search forms currently link directly to results.jsp.
+We'll need to move some things around before this will work.
+*/
 public class resultServlet extends HttpServlet
 {
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOExcepton
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOExcepton
 	{
 		String make = request.getParameter("make");
 		String model = request.getParameter("model");
@@ -23,11 +26,9 @@ public class resultServlet extends HttpServlet
 			{
 				query += "SELECT * FROM vehicle where maker=" + make;
 			}
-			/**
-			 *
-			 * need to output html 
-			 * */
-
+			// Construct a LinkedList of Vehicles
+			// Call request.setAttribute("results", <constructed list>)
+			this.getServletContext().getRequestDispatcher("/results.jsp").include(request, response);
 		}	
 		catch(SQLException e)
 		{
