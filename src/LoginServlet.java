02dccc02e52307efa1ad3495d9d689f2585b7cb6
Mason-Chou <masonchou95035@gmail.com>
2015-05-08 21:18:07 -0700

    Update LoginServlet.java

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -9,14 +9,13 @@ public class LoginServlet extends HttpServlet
 {
     private String user;
     private String pass;
-    
+    private int id;
     public void init() throws ServletException
     {
         user = null;
         pass = null;
     }
     
-    //Does validation of user & password, then fowards it to the request
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
     {
         user = request.getParameter("user");
@@ -29,9 +28,13 @@ public class LoginServlet extends HttpServlet
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/car_match", "root", "password");
             Statement s = con.createStatement();
-            ResultSet rs = s.executeQuery("Select * from customer where UserName='" + user + "' AND Password='" + pass + "'");
+            ResultSet rs = s.executeQuery("Select * from  car_match.access where UserName='" + user + "' AND Password='" + pass + "'");
             if(rs.next())
+            {
                 valid = true;
+                user = rs.getString("UserName");
+                id = rs.getInt("User");
+            }
             else
                 valid = false;
             
@@ -52,9 +55,12 @@ public class LoginServlet extends HttpServlet
         
         if(valid)
         {
-            request.setAttribute("user", user);
-            RequestDispatcher dispatcher = request.getRequestDispatcher("");
-            dispatcher.forward(request, response);
+           HttpSession session = request.getSession();
+           session.setAttribute("user", user);
+           session.setAttribute("id", id);
+           session.setMaxInactiveInterval(30*60);
+           String encodedURL = response.encodeRedirectURL("index.jsp");
+           response.sendRedirect(encodedURL);
         }
         
         else
